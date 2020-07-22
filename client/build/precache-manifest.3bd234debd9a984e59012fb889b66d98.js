ed969c4ea95148418007a6e1770bdeedfecebfe8
MeetPanchani <mmeetpanchani63@gmail.com>
2020-07-22 11:44:29 +0530

    change client dir

---
+++ client/build/precache-manifest.3bd234debd9a984e59012fb889b66d98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64675f15880721ea044590d6075553a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "759023ffc63aac01af9e",
+    "url": "/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "759023ffc63aac01af9e",
+    "url": "/static/js/2.6ae3dcee.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.6ae3dcee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde7aa48b2b949026e72",
+    "url": "/static/js/main.290ae35a.chunk.js"
+  },
+  {
+    "revision": "a37af620878b53014277",
+    "url": "/static/js/runtime-main.0af7428b.js"
+  }
+]);
--- server.js
+++ server.js
@@ -23,6 +23,14 @@ connection.once('open', () => {
 app.use('/exercises', exerciseRouter)
 app.use('/users', userRouter)
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build/'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
